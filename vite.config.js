af7d1799868accb0ad27f5e6461971d791e0f727
EdwardMan917 <edwardman917@gmail.com>
2022-11-02 05:31:35 +0800

    conf: update config base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ssss-demo",
   resolve: {
     alias: {
       process: "process/browser",
